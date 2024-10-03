5b20cf77d8499d3a00fd90f0c37711468a1f22f7
Caioh-12 <silva.caioh08@gmail.com>
2024-10-03 15:21:56 +0000

    iniciado o codigo javascript

---
+++ calculadora/js/script.js
@@ -0,0 +1,24 @@
+// faz referencia aos elementos do dom
+const numero1 = document.querySelector('#numero1')
+
+const numero2 = document.querySelector('#numero2')
+
+const btncalcular = document.querySelector('#btn-calcular')
+
+const resultado = document.querySelector('.resultado')
+
+const divisao = document.querySelector('.divisao')
+
+const adicao = document.querySelector('.adicao')
+
+const subtracao = document.querySelector('.subtracao')
+
+const multiplicacao = document.querySelector('.multiplicacao')
+
+
+
+
+// cria a função calcular
+
+
+// adiciona escutar de eventos
